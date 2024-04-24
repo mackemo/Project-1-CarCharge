a89ccd8fa98068097d60195918a4ae6078edc5ed
mackemo <mackenzielmoore14@gmail.com>
2024-04-24 17:19:58 -0400

    incorporated geo url, got rid of objects with lats and lons stored, got rid of buttons on modal in html for lat and lon input, changed input to city

--- assets/script.js
+++ assets/script.js
@@ -1,89 +1,55 @@
-const searchBtn = document.getElementById('search-btn');
-const apiKey = `GSR7zoBZ7gl3FAdkEk2rHsu7C85lXVHL`;
-const city = JSON.parse(localStorage.getItem('search-input')) || [];
-
-
-const memphis = document.getElementById('memphis')
-const nashville = document.getElementById('nashville')
-const knoxville = document.getElementById('knoxville')
-const chattanooga = document.getElementById('chattanooga')
+const searchInput = document.querySelector(`#city`);
+const formEl = document.getElementById('submit');
+const searchButton = document.querySelector('#search-button');
 const resultsDiv = document.getElementById("results");
-const originalBrightness = document.body.style.filter;
+const apiKey = `GSR7zoBZ7gl3FAdkEk2rHsu7C85lXVHL`;
 
+function geocodeCity(city) {
+    const geoUrl = `https://api.tomtom.com/search/2/geocode/${city}.json?key=${apiKey}`;
 
-searchBtn.addEventListener('click', function(e) {
-    e.preventDefault();
-
-    let searchInput = document.getElementById('search-input').value;
-    city.push(searchInput);
-    localStorage.setItem('search-input', JSON.stringify(city));
-
-    searchAPI(city);
-    
-});
-
-function searchAPI(city) {
-
-    const geoURL = `https://api.tomtom.com/search/2/geocode/${city}.json?key=${apiKey}`;
-
-    fetch(geoURL)
+    fetch(geoUrl)
         .then(function (response) {
             return response.json();
         })
         .then(function (geoResults) {
             console.log(geoResults);
-            const lat = geoResults.results[0].position.lat;
-            const lon = geoResults.results[0].position.lon;
-            console.log(lat);
-            console.log(lon);
-
-            const tomUrl = `https://api.tomtom.com/traffic/services/4/flowSegmentData/absolute/10/json?key=${apiKey}&unit=mph&point=${lat},${lon}`;
-
-            fetch(tomUrl)
-                .then(function (response) {
-                    return response.json();
-                })
-                .then(function (tomData) {
-                    console.log(tomData);
-                    localStorage.setItem('locationData', JSON.stringify(data));
-                    window.location.href = "data.html"
-                })
-
-            const ocURL = `https://api.openchargemap.io/v3/poi/?output=json&maxresults=20&key=789f86d1-a5b2-4530-8ca0-fa64aebcc952&latitude=${lat}&longitude=${lon}`;
-    
-            fetch(ocURL)
-                .then(function (response) {
-                    return response.json();
-                })
-                .then(function (ocData) {
-                    console.log(ocData);
-                    localStorage.setItem('userData', JSON.stringify(data));
-                })
+            const latitude = geoResults.results[0].position.lat;
+            const longitude = geoResults.results[0].position.lon;
+            apiCall(latitude, longitude);
+            apiCall1(latitude, longitude);
         })
 }
 
-// Saved coordinated for 4 cities
-memphis.addEventListener("click", function (event) {
+formEl.addEventListener("submit", function (event) {
     event.preventDefault();
+    const city = searchInput.value;
+    geocodeCity(city);
+});
+
+// function for OpenCharge API
+function apiCall1(lat, lon) {
+    const baseUrl = `https://api.openchargemap.io/v3/poi/?output=json&countrycode=US&stateorprovince=TN&maxresults=20?key=789f86d1-a5b2-4530-8ca0-fa64aebcc952&latitude=${lat}&longitude=${lon}`
     
+    fetch(baseUrl).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        console.log(data)
+        // Local Storage
+        localStorage.setItem('locationData', JSON.stringify(data));
+        window.location.href = "data.html"
+    });
 
-})
+} 
 
-nashville.addEventListener("click", function (event) {
-    event.preventDefault();
-    l
+// function for TomTom API
+function apiCall(lat, lon) {
+    const apiUrl = `https://api.tomtom.com/traffic/services/4/flowSegmentData/absolute/10/json?key=${apiKey}&point=${lat},${lon}&unit=mph`
 
-})
-
-knoxville.addEventListener("click", function (event) {
-    event.preventDefault();
-    
-
-})
-
-chattanooga.addEventListener("click", function (event) {
-    event.preventDefault();
-    
-
-})
-
+    fetch(apiUrl).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        console.log(data)
+        // Local Storage
+        localStorage.setItem('userData', JSON.stringify(data));
+    });
+}
