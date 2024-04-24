4a235c7381e2616ee87fd40972eddc5e0217aa01
mackemo <mackenzielmoore14@gmail.com>
2024-04-24 12:33:06 -0400

    changed lat and lon input to city input, added geocoding url and fetched for lat and lon that is inputted into tomtom and Opencharge

--- assets/script.js
+++ assets/script.js
@@ -1,81 +1,89 @@
-const searchInput = document.querySelector(`#lon`, `#lat`)
-const formEl = document.getElementById('submit')
-const searchButton = document.querySelector('#search-button')
-const resultsDiv = document.getElementById("results");
-const apiKey = `GSR7zoBZ7gl3FAdkEk2rHsu7C85lXVHL`
-const lat = document.getElementById('lat')
-const lon = document.getElementById('lon')
+const searchBtn = document.getElementById('search-btn');
+const apiKey = `GSR7zoBZ7gl3FAdkEk2rHsu7C85lXVHL`;
+const city = JSON.parse(localStorage.getItem('search-input')) || [];
+
+
 const memphis = document.getElementById('memphis')
 const nashville = document.getElementById('nashville')
 const knoxville = document.getElementById('knoxville')
 const chattanooga = document.getElementById('chattanooga')
+const resultsDiv = document.getElementById("results");
 const originalBrightness = document.body.style.filter;
 
-// function for OpenCharge API
-function apiCall1(lat, lon) {
-    const baseUrl = `https://api.openchargemap.io/v3/poi/?output=json&countrycode=US&stateorprovince=TN&maxresults=20?key=789f86d1-a5b2-4530-8ca0-fa64aebcc952&latitude=${lat}&longitude=${lon}`
+
+searchBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    let searchInput = document.getElementById('search-input').value;
+    city.push(searchInput);
+    localStorage.setItem('search-input', JSON.stringify(city));
+
+    searchAPI(city);
     
-    fetch(baseUrl).then(function (response) {
-        console.log(response);
-        return response.json();
-    }).then(function (data) {
-        console.log(data)
-        // Local Storage
-        localStorage.setItem('locationData', JSON.stringify(data));
-        window.location.href = "data.html"
-    });
-
-} 
-
-// function for TomTom API
-function apiCall(param1, param2) {
-    const apiUrl = `https://api.tomtom.com/traffic/services/4/flowSegmentData/absolute/10/json?key=${apiKey}&point=${param1},${param2}&unit=mph`
-
-    fetch(apiUrl).then(function (response) {
-        console.log(response);
-        return response.json();
-    }).then(function (data) {
-        console.log(data)
-        // Local Storage
-        localStorage.setItem('userData', JSON.stringify(data));
-    });
-}
-
-// Event Listener for Search Button
-formEl.addEventListener("submit", function (event) {
-    event.preventDefault();
-    const longitude = lon.value
-    const latitude = lat.value
-    apiCall(latitude, longitude) // Call functions
-    apiCall1(latitude, longitude)
 });
+
+function searchAPI(city) {
+
+    const geoURL = `https://api.tomtom.com/search/2/geocode/${city}.json?key=${apiKey}`;
+
+    fetch(geoURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (geoResults) {
+            console.log(geoResults);
+            const lat = geoResults.results[0].position.lat;
+            const lon = geoResults.results[0].position.lon;
+            console.log(lat);
+            console.log(lon);
+
+            const tomUrl = `https://api.tomtom.com/traffic/services/4/flowSegmentData/absolute/10/json?key=${apiKey}&unit=mph&point=${lat},${lon}`;
+
+            fetch(tomUrl)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (tomData) {
+                    console.log(tomData);
+                    localStorage.setItem('locationData', JSON.stringify(data));
+                    window.location.href = "data.html"
+                })
+
+            const ocURL = `https://api.openchargemap.io/v3/poi/?output=json&maxresults=20&key=789f86d1-a5b2-4530-8ca0-fa64aebcc952&latitude=${lat}&longitude=${lon}`;
+    
+            fetch(ocURL)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (ocData) {
+                    console.log(ocData);
+                    localStorage.setItem('userData', JSON.stringify(data));
+                })
+        })
+}
 
 // Saved coordinated for 4 cities
 memphis.addEventListener("click", function (event) {
     event.preventDefault();
-    lon.value = -90.0490
-    lat.value = 35.1495
+    
 
 })
 
 nashville.addEventListener("click", function (event) {
     event.preventDefault();
-    lon.value = -86.7816
-    lat.value = 36.1627
+    l
 
 })
 
 knoxville.addEventListener("click", function (event) {
     event.preventDefault();
-    lon.value = -83.9207
-    lat.value = 35.9606
+    
 
 })
 
 chattanooga.addEventListener("click", function (event) {
     event.preventDefault();
-    lon.value = -85.3094
-    lat.value = 35.0458
+    
 
 })
 
